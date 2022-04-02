9ab92b9a1121131b31038a901732e30bf377211f
orestis tzourmpakis <otzurbakis@hotmail.com>
2022-04-02 13:23:02 +0300

    - final categories

--- Server.Application/Features/Sales/Queries/GetActiveSalesByCompanyIdQuery.cs
+++ Server.Application/Features/Sales/Queries/GetActiveSalesByCompanyIdQuery.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
 using Server.Application.Contracts;
 using Server.Application.Responses;
+using Server.Domain.Models;
 
 namespace Server.Application.Features.Sales.Queries
 {
@@ -32,9 +34,15 @@ namespace Server.Application.Features.Sales.Queries
         {
             // i have the company id so go to the sales table!!
             var activeSales = await _unitOfWork.Sales.GetAsync(c => c.CompanyId == request.Id && c.DateEnd > DateTime.Now, disableTracking: true);
+            var includeListCategory = new List<Expression<Func<Company, object>>>() { c => c.Category };
+
+            var category = await _unitOfWork.Companies.GetByIdAsync(c => c.Id == request.Id, includes: includeListCategory);
+            var finalCategory = category.Category != null ? category.Category.Name : null;
+
             var finalSales = activeSales.Select(a =>
             {
                 a.Image = a.Image == null ? null : _httpContextAccessorWrapper.GetUrl() + "Images/" + a.Image;
+                a.Category = finalCategory;
                 return a;
             });
             return new BaseResponse
--- Server.Domain/Models/Sales.cs
+++ Server.Domain/Models/Sales.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,8 @@ namespace Server.Domain.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
+        [NotMapped]
+        public string Category { get; set; }
         public DateTime DateStart { get; set; }
         public DateTime DateEnd { get; set; }
         public int CompanyId { get; set; }
